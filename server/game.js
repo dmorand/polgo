7694ea0c7512b34628f07472ba5d14d3a94c7300
David Morand <dmorand@nuecho.com>
2015-09-29 09:24:14 -0400

    - basic move validation - endpoints for board and moves

--- server/game.js
+++ server/game.js
@@ -2,9 +2,24 @@
 
 const Move = require('./move.js');
 
+const SIZE = 8;
+
+function outbounds(x, y) {
+  return x < 0 || x >= SIZE || y < 0 || y >= SIZE;
+}
+
+function occupied(board, x, y) {
+  return board[x][y] !== undefined;
+}
+
 function move(moves, board, x, y, color) {
-  board[x][y] = color;
   moves.push(new Move(x, y, color));
+
+  if (outbounds(x, y)) return false;
+  if (occupied(board, x, y)) return false;
+
+  board[x][y] = color;
+  return true;
 }
 
 module.exports = class {
@@ -13,30 +28,29 @@ module.exports = class {
     this.moves = [];
     this.board = [];
 
-    for (let x = 0; x < 8; x++) {
-      const row = [];
-      this.board.push(row);
-      for (let y = 0; y < 8; y++) {
-        row.push('.');
-      }
+    for (let x = 0; x < SIZE; x++) {
+      this.board.push([]);
     }
   }
 
   black(x, y) {
-    move(this.moves, this.board, x, y, 'B');
+    return move(this.moves, this.board, x, y, 'B');
   }
 
   white(x, y) {
-    move(this.moves, this.board, x, y, 'W');
+    return move(this.moves, this.board, x, y, 'W');
   }
 
   render() {
     let renderedBoard = '';
+    const board = this.board;
 
-    for (let x = 0; x < 8; x++) {
-      for (let y = 0; y < 8; y++) {
-        renderedBoard += this.board[x][y];
+    for (let x = 0; x < SIZE; x++) {
+      for (let y = 0; y < SIZE; y++) {
+        const tile = this.board[x][y];
+        renderedBoard += tile ? tile : '.';
       }
+
       renderedBoard += '\n';
     }
 
--- server/server.js
+++ server/server.js
@@ -5,15 +5,32 @@ const Game = require('./game.js');
 const server = Express();
 const games = {};
 
+function findGame(request, response) {
+  const game = games[request.params.id];
+
+  if (!game) {
+    response.status(404).end();
+  }
+
+  return game;
+}
+
+function renderGame(game, response) {
+  response.set('Content-Type', 'text/plain');
+  response.send(game.render());
+}
+
 server.get('/', function(request, response) {
+  var version = '0.0.1';
+  var status = 'online';
+
   response.json({
-    status: 'online'
+    version, status
   });
 });
 
 server.get('/game', function(request, response) {
   const uuid = Uuid.v4();
-
   games[uuid] = new Game();
 
   response.json({
@@ -21,44 +38,38 @@ server.get('/game', function(request, response) {
   });
 });
 
-server.get('/game/:id', function(request, response) {
-  const game = games[request.params.id];
+server.get('/game/:id/board', function(request, response) {
+  const game = findGame(request, response);
+  if (!game) return;
 
-  if (!game) {
-    response.status(404).end();
-    return;
-  }
-
-  response.set('Content-Type', 'text/plain');
-  response.send(game.render());
+  renderGame(game, response);
 });
 
-server.get('/game/:id/:x/:y/white', function(request, response) {
-  const game = games[request.params.id];
+server.get('/game/:id/moves', function(request, response) {
+  const game = findGame(request, response);
+  if (!game) return;
 
-  if (!game) {
-    response.status(404).end();
-    return;
-  }
-
-  game.white(request.params.x, request.params.y);
-
-  response.set('Content-Type', 'text/plain');
-  response.send(game.render());
+  response.json(game.moves);
 });
 
-server.get('/game/:id/:x/:y/black', function(request, response) {
-  const game = games[request.params.id];
+server.get('/game/:id/black/:x/:y', function(request, response) {
+  const game = findGame(request, response);
+  if (!game) return;
 
-  if (!game) {
-    response.status(404).end();
-    return;
-  }
+  const legal = game.black(request.params.x, request.params.y);
+  response.json({
+    legal
+  });
+});
 
-  game.black(request.params.x, request.params.y);
+server.get('/game/:id/white/:x/:y', function(request, response) {
+  const game = findGame(request, response);
+  if (!game) return;
 
-  response.set('Content-Type', 'text/plain');
-  response.send(game.render());
+  const legal = game.white(request.params.x, request.params.y);
+  response.json({
+    legal
+  });
 });
 
 const port = process.argv[2];
