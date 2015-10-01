5794cf004eec8f45e9c95aba91c24879142a3421
David Morand <dmorand@nuecho.com>
2015-09-30 22:50:20 -0400

    games are saved on disk after each move

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
 const Package = require('./package.json');
 const Config = require('./config.json');
 
+const FileSystem = require('fs');
 const Express = require('express');
 const Uuid = require('uuid');
+
 const Game = require('./game.js');
 
 const server = Express();
 const games = {};
 
 function findGame(request, response) {
-  const game = games[request.params.id];
+  const game = games[request.params.uuid];
 
   if (!game) {
     response.status(404).end();
@@ -21,6 +23,11 @@ function findGame(request, response) {
 function renderGame(game, response) {
   response.set('Content-Type', 'text/plain');
   response.send(game.render());
+}
+
+function writeGame(uuid, game) {
+  const filename = Config.gameDirectory + '/' + uuid + '.json';
+  FileSystem.writeFileSync(filename, JSON.stringify(game.moves, null, '  '));
 }
 
 server.get('/', function(request, response) {
@@ -36,47 +43,64 @@ server.get('/game/list', function(request, response) {
 
 server.get('/game/create', function(request, response) {
   const uuid = Uuid.v4();
-  games[uuid] = new Game();
+  const game = new Game();
+
+  games[uuid] = game;
+  writeGame(uuid, game);
 
   response.json({
     uuid
   });
 });
 
-server.get('/game/:id/board', function(request, response) {
+server.get('/game/:uuid/board', function(request, response) {
   const game = findGame(request, response);
   if (!game) return;
 
   renderGame(game, response);
 });
 
-server.get('/game/:id/moves', function(request, response) {
+server.get('/game/:uuid/moves', function(request, response) {
   const game = findGame(request, response);
   if (!game) return;
 
   response.json(game.moves);
 });
 
-server.get('/game/:id/black/:x/:y', function(request, response) {
+server.get('/game/:uuid/black/:x/:y', function(request, response) {
   const game = findGame(request, response);
   if (!game) return;
 
   const legal = game.black(request.params.x, request.params.y);
+
+  if (legal) {
+    writeGame(request.params.uuid, game);
+  }
+
   response.json({
     legal
   });
 });
 
-server.get('/game/:id/white/:x/:y', function(request, response) {
+server.get('/game/:uuid/white/:x/:y', function(request, response) {
   const game = findGame(request, response);
   if (!game) return;
 
   const legal = game.white(request.params.x, request.params.y);
+
+  if (legal) {
+    writeGame(request.params.uuid, game);
+  }
+
   response.json({
     legal
   });
 });
 
 server.listen(Config.port, function() {
+  if (!FileSystem.existsSync(Config.gameDirectory)) {
+    FileSystem.mkdirSync(Config.gameDirectory);
+  }
+
   console.log('Polgo server started on port %s', Config.port);
 });
