7e79e0231d0ac946f12e61a98a9de155c9dc335a
David Morand <dmorand@nuecho.com>
2015-11-08 21:08:38 -0500

    added basic tests + Travis CI integration

---
+++ server/test/move.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+const Move = require('../move.js');
+
+describe("Move", function() {
+  it("is properly created.", function() {
+    const move = new Move(3, 6, 'W');
+    expect(move.x).to.equal(3);
+    expect(move.y).to.equal(6);
+    expect(move.color).to.equal('W');
+  });
+});
